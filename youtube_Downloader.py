95a6be52f911d1afb9e07be4c12d7261f152800d
Jaikeesoni <jaisoni280284@gmail.com>
2024-05-25 14:22:50 +0530

    Update youtube_Downloader.py

--- youtube_Downloader.py
+++ youtube_Downloader.py
@@ -20,7 +20,7 @@ if url:
     selected_stream = stream.get_by_itag(selected_option[0])
     safe_filename = yt.title.replace('/', '-').replace('\\', '-').replace(':', '-').replace('|', '-').replace('*', '-').replace('?', '-').replace('"', '-').replace('<', '-').replace('>', '-')
 
-    with tempfile.NamedTemporaryFile(delete=True) as tmpfile:
+    with tempfile.NamedTemporaryFile(delete=False) as tmpfile:
         try:
                 # Download the video directly to a temporary file
             selected_stream.download(output_path=os.path.dirname(tmpfile.name), filename=safe_filename)
@@ -31,7 +31,7 @@ if url:
                 st.download_button(label='Download Video',
                                data=f,
                                file_name=safe_filename + ".mp4",
-                               mime="video/mp4",on_click=False)
+                               mime="video/mp4")
         except Exception as e:
             st.error(f"Failed to download the video: {e}")
         finally:
